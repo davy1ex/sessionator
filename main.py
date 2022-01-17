099557942391138fa6f9d252369cb212ceeac00d
davy1ex <32157455+davy1ex@users.noreply.github.com>
2022-01-17 07:56:21 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import random
 from PIL import Image
 
 
-images = os.listdir()
+images = os.listdir(os.path.join(os.getcwd(), 'imgs'))
 random.shuffle(images)
 result = []
 
 
 for i in images:
 
-    with Image.open(i) as img:
+    with Image.open(os.path.join('imgs', i)) as img:
         img.show()
         result.append(str(i) + ": " + input())
         img.close()
